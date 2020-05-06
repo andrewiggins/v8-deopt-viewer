a21dfd4a3571dac73db99205acc7da1885a74e8e
Andre Wiggins <andrewiggins@live.com>
2020-05-06 14:51:37 -0700

    Add stubs for v8-deopt-parser

---
+++ packages/v8-deopt-parser/src/index.d.ts
@@ -0,0 +1,14 @@
+interface V8DeoptInfo {
+  // TODO
+}
+
+/**
+ * Parse the deoptimizations from a v8.log file
+ * @param v8LogContent The contents of a v8.log file
+ * @param readSourceFileContent Given paths to source files from the v8.log,
+ * this function should return their contents
+ */
+export async function parseV8Log(
+  v8LogContent: string,
+  readSourceFileContent: (srcFilePath: string) => Promise<string>
+): V8DeoptInfo;
---
+++ packages/v8-deopt-parser/src/index.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} v8LogContent
+ * @param {(srcFilePath: string) => Promise<string>} readSourceFileContent
+ */
+export async function parseV8Log(v8LogContent, readSourceFileContent) {
+  //
+}
