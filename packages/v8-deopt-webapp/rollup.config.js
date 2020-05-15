fcfe1c309f7e2e433d96c7cdc44cd7b53ef21a14
Andre Wiggins <andrewiggins@live.com>
2020-05-15 16:20:07 -0700

    Add minification to webapp

--- packages/v8-deopt-webapp/rollup.config.js
+++ packages/v8-deopt-webapp/rollup.config.js
@@ -3,37 +3,50 @@ import commonjs from "@rollup/plugin-commonjs";
 import sucrase from "@rollup/plugin-sucrase";
 import postcss from "rollup-plugin-postcss";
 import visualizer from "rollup-plugin-visualizer";
+import { terser } from "rollup-plugin-terser";
 
-/** @type {import('rollup').RollupOptions} */
-const config = {
-	input: "./src/index.js",
-	output: {
-		dir: "dist",
-		format: "esm",
-	},
-	plugins: [
-		nodeResolve(),
-		sucrase({
-			include: [/.js$/],
-			production: true,
-			transforms: ["jsx"],
-			jsxPragma: "createElement",
-		}),
-		postcss({
-			extract: true,
-			modules: true,
-			minimize: true,
-			namedExports(name) {
-				return name.replace(/-/g, "_");
-			},
-		}),
-		commonjs(),
+/**
+ * @param {string} outputFile
+ * @param {any[]} customPlugins
+ * @returns {import('rollup').RollupOptions}
+ */
+function getConfig(outputFile, customPlugins) {
+	return {
+		input: "./src/index.js",
+		output: {
+			file: outputFile,
+			format: "esm",
+		},
+		plugins: [
+			nodeResolve(),
+			sucrase({
+				include: [/.js$/],
+				production: true,
+				transforms: ["jsx"],
+				jsxPragma: "createElement",
+			}),
+			postcss({
+				extract: true,
+				modules: true,
+				minimize: true,
+				namedExports(name) {
+					return name.replace(/-/g, "_");
+				},
+			}),
+			commonjs(),
+			...customPlugins,
+		],
+	};
+}
+
+export default [
+	getConfig("dist/index.debug.js", []),
+	getConfig("dist/index.js", [
+		terser(),
 		visualizer({
 			filename: "dist/stats.html",
 			gzipSize: true,
 			// brotliSize: true,
 		}),
-	],
-};
-
-export default config;
+	]),
+];
