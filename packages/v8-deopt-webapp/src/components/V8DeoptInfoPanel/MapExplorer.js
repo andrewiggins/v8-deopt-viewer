123a39c846e1140b567c4ea20c71394690842264
Andre Wiggins <andrewiggins@live.com>
2020-06-19 23:32:34 -0700

    Add map grouping select data

--- packages/v8-deopt-webapp/src/components/V8DeoptInfoPanel/MapExplorer.js
+++ packages/v8-deopt-webapp/src/components/V8DeoptInfoPanel/MapExplorer.js
@@ -1,24 +1,71 @@
 import { createElement, Fragment } from "preact";
-import { map_selectors, grouping, group_value } from "./MapExplorer.scss";
-import { form_group, form_select } from "../../spectre.scss";
+import { useState } from "preact/hooks";
+import {
+	map_selectors,
+	grouping as map_grouping,
+	group_value,
+} from "./MapExplorer.scss";
+import { form_group, form_select, form_label } from "../../spectre.scss";
+
+/**
+ * @typedef {"create" | "loadic" | "property" | "mapid"} MapGrouping
+ * @type {Record<MapGrouping, { label: string; valueLabel: string; value: MapGrouping }>}
+ */
+const mapGroupings = {
+	create: {
+		label: "Creation location",
+		valueLabel: "Location",
+		value: "create",
+	},
+	loadic: {
+		label: "LoadIC location",
+		valueLabel: "Location",
+		value: "loadic",
+	},
+	property: {
+		label: "Property name",
+		valueLabel: "Property",
+		value: "property",
+	},
+	mapid: {
+		label: "Map ID",
+		valueLabel: "ID",
+		value: "mapid",
+	},
+};
 
 /**
  * @param {{ urlBase: string }} props
  */
 export function MapExplorer({ urlBase }) {
+	/** @type {[MapGrouping, import('preact/hooks').StateUpdater<MapGrouping>]} */
+	const [grouping, setGrouping] = useState("create");
+
 	return (
 		<Fragment>
 			<div class={map_selectors}>
-				<div class={[form_group, grouping].join(" ")}>
-					<select class={form_select}>
-						<option>Choose an option</option>
-						<option>Slack</option>
-						<option>Skype</option>
-						<option>Hipchat</option>
+				<div class={[form_group, map_grouping].join(" ")}>
+					<label for="map-grouping" class={form_label}>
+						Map grouping:
+					</label>
+					<select
+						value={grouping}
+						onChange={(e) => setGrouping(e.currentTarget.value)}
+						id="map-grouping"
+						class={form_select}
+					>
+						{Object.values(mapGroupings).map((group) => (
+							<option key={group.value} value={group.value}>
+								{group.label}
+							</option>
+						))}
 					</select>
 				</div>
 				<div class={[form_group, group_value].join(" ")}>
-					<select class={form_select}>
+					<label for="map-group" class={form_label}>
+						{mapGroupings[grouping].valueLabel}:
+					</label>
+					<select id="map-group" class={form_select}>
 						<option>Choose an option</option>
 						<option>Slack</option>
 						<option>Skype</option>
